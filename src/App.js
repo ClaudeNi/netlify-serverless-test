55b4274947b2fdacb19bf8f52adfda384549ca6d
Claude Ni <49127719+ClaudeNi@users.noreply.github.com>
2022-01-25 10:19:33 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import {useState} from 'react'
-import axios from axios;
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [text, setText] = useState("Have no data fetched");
+    const [text, setText] = useState("Have no data fetched");
 
-  const fetchData = () => {
-    const fetchedData = await axios.get("/test");
-    setText(fetchedData.data);
-  }
-  
-  return (
-    <div className="App">
-      <button onClick={fetchData}>Fetch Data</button>
-      {text}
-    </div>
-  );
+    const fetchData = () => {
+        const fetchedData = await axios.get("/test");
+        setText(fetchedData.data);
+    };
+
+    return (
+        <div className="App">
+            <button onClick={fetchData}>Fetch Data</button>
+            {text}
+        </div>
+    );
 }
 
 export default App;
